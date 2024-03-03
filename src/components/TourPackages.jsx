bd3c1dd3296cd5c4dd3eca8f9a72dbe8f96dfa8f
Psudopodiya <chinmay.raiker2002@gmail.com>
2024-03-03 12:54:08 +0530

    changes in the simaple tours

--- src/components/TourPackages.jsx
+++ src/components/TourPackages.jsx
@@ -1,9 +1,21 @@
 import ArrowCircleLeftIcons from '@mui/icons-material/ArrowCircleLeft';
 import ArrowCircleRightIcon from '@mui/icons-material/ArrowCircleRight';
+import { useState } from 'react';
 
 import CardList from '../components/UI/CardList';
 
 const TourPackages = () => {
+    const [scrollX, setScrollX] = useState(0);
+
+    const handleScrollLeft = () => {
+        setScrollX((prevScrollX) => Math.min(prevScrollX + 300, 0));
+    };
+
+    const handleScrollRight = () => {
+        setScrollX((prevScrollX) =>
+            Math.max(prevScrollX - 300, -3000 + window.innerWidth),
+        );
+    };
     return (
         <div className="bg-[#F3F7FC] px-40 py-28 font-Manrope">
             <div className="flex flex-row gap-5 ">
@@ -19,19 +31,28 @@ const TourPackages = () => {
                 </div>
                 <div className="flex items-center justify-center gap-5 ">
                     <ArrowCircleLeftIcons
+                        onClick={handleScrollLeft}
                         sx={{
                             '&:hover': { color: '#009ECA' },
+                            fontSize: '32px',
                         }}
                     />
                     <ArrowCircleRightIcon
+                        onClick={handleScrollRight}
                         sx={{
                             '&:hover': { color: '#009ECA' },
+                            fontSize: '32px',
                         }}
                     />
                 </div>
             </div>
             <div>
-                <CardList />
+                <div
+                    className="transition-transform duration-500"
+                    style={{ transform: `translateX(${scrollX}px)` }}
+                >
+                    <CardList /> {/* This should render your cards */}
+                </div>
             </div>
         </div>
     );
