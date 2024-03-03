d47549daf11889adc55ca05edf1e259c1f71d95a
Psudopodiya <chinmay.raiker2002@gmail.com>
2024-03-03 13:02:55 +0530

    trying vercel analytics and speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import './App.css';
 
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Import these
 
 import HomePage from './pages/HomePage';
@@ -8,6 +10,8 @@ import PlanTrip from './pages/PlanTrip';
 function App() {
     return (
         <BrowserRouter>
+            <SpeedInsights />
+            <Analytics />
             <Routes>
                 <Route path="/" element={<HomePage />} />
                 <Route path="/plan-your-trip" element={<PlanTrip />} />
